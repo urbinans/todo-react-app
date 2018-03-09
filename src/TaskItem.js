7d5f8406cabb7f793cb7e49eb20349df4a7fcef5
Nelson Urbina <urbinans@gmail.com>
2018-03-08 22:01:50 -0400

    Del button activated

--- src/TaskItem.js
+++ src/TaskItem.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react';
 import './TaskItem.css';
 
 class TaskItem extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDel = this.handleDel.bind(this);
+  }
+
+  handleDel() {
+    this.props.handleDel(this.props.taskId);
+  }
+
   render() {
     return (
       <div className="Task">
         <input type="checkbox" defaultChecked={this.props.checked} />
         <div className="Task-actions">
           <i className="fas fa-pencil-alt Task-edit"></i>
-          <i className="fas fa-trash-alt Task-delete"></i>
+          <i className="fas fa-trash-alt Task-delete" onClick={this.handleDel}></i>
         </div>
         <span className="Task-desc">{this.props.description}</span>
       </div>
--- src/TaskLister.js
+++ src/TaskLister.js
@@ -3,10 +3,25 @@ import './TaskLister.css';
 import TaskItem from './TaskItem';
 
 class TaskLister extends Component {
+  constructor(props) {
+    super(props);
+    this.handleDel = this.handleDel.bind(this);
+  }
+
+  handleDel(taskId){
+    this.props.handleDel(taskId);
+  }
+
   render() {
     const items = this.props.tasks.map(task => {
       return (
-        <TaskItem key={task.id} checked={task.done} description={task.description} />
+        <TaskItem
+          key={task.id}
+          taskId={task.id}
+          checked={task.done}
+          description={task.description}
+          handleDel={this.handleDel}
+        />
       );
     });
 
