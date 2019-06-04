2a0f8571e9f4a0b224b26535701444dd5ffc42c0
Nelson Urbina <urbinans@gmail.com>
2019-06-03 21:55:48 -0400

    Updates

---
+++ precache-manifest.4f16636a333c94564fdc926ec507becb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577e523917c48ea21e44b930fad3c0ef",
+    "url": "/todo-react-app/index.html"
+  },
+  {
+    "revision": "d08452ccc719048bde22",
+    "url": "/todo-react-app/static/css/main.0205eee0.chunk.css"
+  },
+  {
+    "revision": "c7912aa8bc4d2cbfd529",
+    "url": "/todo-react-app/static/js/2.8611b558.chunk.js"
+  },
+  {
+    "revision": "d08452ccc719048bde22",
+    "url": "/todo-react-app/static/js/main.61cc0bf4.chunk.js"
+  },
+  {
+    "revision": "5fd7fe22f3e7a4aea29a",
+    "url": "/todo-react-app/static/js/runtime~main.7e9341e7.js"
+  }
+]);
